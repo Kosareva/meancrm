1316db5c99447b07d0620409cb6273381f8207e4
kosareva962 <kosareva962@gmail.com>
2019-02-27 14:15:12 +0200

    created routes, controllers, models; mongoDb initialized

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const keys = require('./config/keys');
+const authRoutes = require('./routes/auth');
+const analyticsRoutes = require('./routes/analytics');
+const categoryRoutes = require('./routes/category');
+const orderRoutes = require('./routes/order');
+const positionRoutes = require('./routes/position');
+const app = express();
+
+mongoose.connect(keys.mongoDbConnectionString)
+    .then(() => console.log('Mongose connected'))
+    .catch(e => console.log('Can\'t connect to mongoose', e));
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/analytics', analyticsRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/order', orderRoutes);
+app.use('/api/position', positionRoutes);
+
+module.exports = app;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoDbConnectionString: 'mongodb+srv://admin:admin@cluster0-8w844.mongodb.net/test?retryWrites=true'
+};
